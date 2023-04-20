8d3d590e871e3ce9745c20f88aa274790359b484
kmfd14 <kmfd14@gmail.com>
2023-04-20 17:41:45 +0800

    add devise views, config username authentication, login, and registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:username, :email, :phone_number, :password, :password_confirmation, :role]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :password, :remember_me)
+      end
+  end
 end

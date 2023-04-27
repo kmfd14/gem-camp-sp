fd268e0dee35ef1bdf80bd04c1e3a87bbe2e00dc
KMFD-LAPTOP\KMFD <kmfd14@gmail.com>
2023-04-27 18:33:02 +0800

    add missing column in users table, coins column

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_102253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "coins", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

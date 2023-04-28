8e617b4b40700b780d02a1195ef6a7c41bbccb6b
kmfd14 <kmfd14@gmail.com>
2023-04-28 17:21:14 +0800

    add counter cache and self join association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  belongs_to :parent, class_name: 'User', counter_cache: :children_member, optional: true
+  has_many :children, class_name: 'User', foreign_key: 'parent_id'
+
   mount_uploader :image, ImageUploader
 
   enum role: { client: 0, admin: 1 }

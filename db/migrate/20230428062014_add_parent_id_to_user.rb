d640316a9a244d40ae7f24d1cced29a3b38f6ea4
kmfd14 <kmfd14@gmail.com>
2023-04-28 17:21:47 +0800

    add parent_id column for feature 7

---
+++ db/migrate/20230428062014_add_parent_id_to_user.rb
@@ -0,0 +1,5 @@
+class AddParentIdToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :parent_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_27_102253) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_062014) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "username", default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_27_102253) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.integer "coins", default: 0
+    t.integer "parent_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

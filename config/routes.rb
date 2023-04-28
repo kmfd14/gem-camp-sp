9e58c66a289af292ec3fd210cd01ba9a7152fb4e
kmfd14 <kmfd14@gmail.com>
2023-04-28 11:23:48 +0800

    edited routes for the client and admin domains

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root 'home#index'
 
   constraints(ClientDomainConstraint.new) do
-    namespace :client do
+    devise_for :users, as: 'client', controllers: {sessions: 'client/sessions'}
+    namespace :client, path: '/' do
       resource :profile, only: [:show, :edit, :update]
     end
   end
 
   constraints(AdminDomainConstraint.new) do
+    devise_for :users, as: 'admin', controllers: {sessions: 'admin/sessions'}
     namespace :admin do
       resources :users
     end
   end
+
+  root 'home#index'
 end

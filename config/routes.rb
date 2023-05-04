7e7f7e604e4ff2e2a18295d7f7d05f59646a6ab4
kmfd14 <kmfd14@gmail.com>
2023-05-04 12:33:26 +0800

    done address api v1

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,18 @@ Rails.application.routes.draw do
     end
   end
   root 'home#index'
+  namespace :api do
+    resources :regions, only: %i[index show], defaults: { format: :json } do
+      resources :provinces, only: :index, defaults: { format: :json }
+    end
+
+    resources :provinces, only: %i[index show], defaults: { format: :json } do
+      resources :cities, only: :index, defaults: { format: :json }
+    end
+
+    resources :cities, only: %i[index show], defaults: { format: :json } do
+      resources :barangays, only: :index, defaults: { format: :json }
+    end
+    resources :barangays, only: %i[index show], defaults: { format: :json }
+  end
 end

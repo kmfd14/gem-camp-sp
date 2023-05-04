77e65e85724fcd437a52489f0acf709b82ae602a
kmfd14 <kmfd14@gmail.com>
2023-05-04 10:54:05 +0800

    add fetch districts and cities in service

--- app/services/ph_location_service.rb
+++ app/services/ph_location_service.rb
@@ -25,4 +25,35 @@ class PhLocationService
       address_province.save
     end
   end
+
+  def fetch_districts
+    request = RestClient.get("#{url}/districts/")
+    data = JSON.parse(request.body)
+    data.each do |district|
+      region = Address::Region.find_by(code: district['regionCode'])
+      address_district = Address::Province.find_or_initialize_by(code: district['code'])
+      address_district.name = district['name']
+      address_district.region = region
+      address_district.save
+    end
+  end
+
+  def fetch_cities
+    request = RestClient.get("#{url}/cities-municipalities/")
+    data = JSON.parse(request.body)
+    data.each do |city|
+      address_city = Address::City.find_or_initialize_by(code: city['code'])
+      address_city.name = city['name']
+      address_city.province = if city['name'] == 'City of Isabela'
+                                Address::Province.find_by_name('Basilan')
+                              elsif city['name'] == 'City of Cotabato'
+                                Address::Province.find_by_name('Maguindanao')
+                              elsif city['districtCode']
+                                Address::Province.find_by_code(city['districtCode'])
+                              else
+                                Address::Province.find_by_code(city['provinceCode'])
+                              end
+      address_city.save
+    end
+  end
 end

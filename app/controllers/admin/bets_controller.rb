50c42316e5f34ade8e523158cd6bed2f447817ea
kmfd14 <kmfd14@gmail.com>
2023-05-11 18:32:53 +0800

    add bet model index view for admin page with search area including scopes, feature 15

--- app/controllers/admin/bets_controller.rb
+++ app/controllers/admin/bets_controller.rb
@@ -1,5 +1,10 @@
 class Admin::BetsController < ApplicationController
   def index
     @bets = Bet.all
+    @bets = @bets.filter_by_serial_number(params[:serial_number]) if params[:serial_number].present?
+    @bets = @bets.filter_by_item_name(params[:item_name]) if params[:name].present?
+    @bets = @bets.filter_by_email(params[:email]) if params[:email].present?
+    @bets = @bets.filter_by_state(params[:state]) if params[:state].present?
+    @bets = @bets.filter_by_date_range(params[:start_date].to_date..params[:end_date].to_date) if params[:start_date].present? && params[:end_date].present?
   end
 end
--- app/models/bet.rb
+++ app/models/bet.rb
@@ -4,6 +4,13 @@ class Bet < ApplicationRecord
 
   after_create :bet_serial_generator
   after_create :deduct_coin
+
+  scope :filter_by_serial_number, -> (serial_number) { where(bets: {serial_number: serial_number} ) }
+  scope :filter_by_item_name, -> (item_name) { where(bets: {item_name: item_name} ) }
+  scope :filter_by_email, -> (user_email) { where(bets: {email: user_email} ) }
+  scope :filter_by_state, -> (state_name) { where(bets: {state: state_name} ) }
+  scope :filter_by_date_range, -> (date_range) { where(bets: {created_at: date_range})}
+
   include AASM
 
   aasm column: :state do

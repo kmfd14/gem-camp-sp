077366765a4bbbcdba1f512ca3fe22e8618bb761
kmfd14 <kmfd14@gmail.com>
2023-05-15 15:18:04 +0800

    fixed bug on displaying serial number of bets user owns on an item

--- app/controllers/client/lottery_controller.rb
+++ app/controllers/client/lottery_controller.rb
@@ -7,7 +7,7 @@ class Client::LotteryController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @user_bets = current_client_user.bets.where(item_id: @item.id).order(created_at: :desc) if params[:item_id].present?
+    @user_bets = current_client_user.bets.where(item_id: @item.id).order(created_at: :desc)
     @bet = Bet.new
   end
 

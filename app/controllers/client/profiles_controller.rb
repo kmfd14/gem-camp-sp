8c9140f4ed72b2dc15f1865f689572effb7485d5
kmfd14 <kmfd14@gmail.com>
2023-05-16 23:14:07 +0800

    fixed bug on display history records on user profile and updated html views and bootstrap

--- app/controllers/client/profiles_controller.rb
+++ app/controllers/client/profiles_controller.rb
@@ -4,9 +4,10 @@ class Client::ProfilesController < ApplicationController
   def show
     @user = current_client_user
     @users = User.all
+    @winner = Winner.where(user_id: @user)
     @order_history = @user.orders.order(created_at: :desc) if params[:history] == 'order_history'
     @lottery_history = @user.bets if params[:history] == 'lottery_history'
-    @winning_history = @user.bets.won if params[:history] == 'winning_history'
+    @winning_history = @winner if params[:history] == 'winning_history'
     @invitation_history = @users.where(parent_id: @user) if params[:history] == 'invitation_history'
     # @user_order_history = current_client_user.orders.where(offer_id: @order.id)
   end
